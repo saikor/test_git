2010f5c181228fc4b4612187e471c3d2bd952eb0
saikor <saikor@qq.com>
2019-11-13 23:11:50 +0800

    add the learn note for temporary

--- test.c
+++ test.c
@@ -18,3 +18,15 @@ int main()
     printf("test git reset: using the git pull to cancel the reset operation\n");
 	return 0;
 }
+
+
+/*
+* git remote add origin git@github.com:michaelliao/learngit.git
+* 1、关联本地仓库和远端仓库
+* 
+* git push -u origin master
+* 2、第一次推送master分支时，加上了-u参数。
+*    Git不但会把本地的master分支内容推送的远程新的master分支，
+*    还会把本地的master分支和远程的master分支关联起来，
+*    在以后的推送或者拉取时就可以简化命令: git push origin master。
+*/
